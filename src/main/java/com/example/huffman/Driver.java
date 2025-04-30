79c426fa088e3d0cf7de952abb054d7cca0941cb
tasneem1010 <tasneemabusra@gmail.com>
2025-05-01 00:26:31 +0300

    Initial commit

---
+++ src/main/java/com/example/huffman/Driver.java
@@ -0,0 +1,19 @@
+package com.example.huffman;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Driver extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
